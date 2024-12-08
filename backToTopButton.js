6e94f74ddb02093e41ba30c01d8cce1a16eb6e3c
Tyler Gast <tgast2@student.cvtc.edu>
2024-12-07 18:10:06 -0600

    New features

---
+++ backToTopButton.js
@@ -0,0 +1,13 @@
+const backToTop = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        backToTop.style.display = 'block';
+    } else {
+        backToTop.style.display = 'none';
+    }
+});
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
