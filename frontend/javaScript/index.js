0b22e28cbbe06060c3033bd5196d5d149b3d212a
Tyler Gast <tgast2@student.cvtc.edu>
2024-12-22 17:15:30 -0600

    More tweaks to fix responsive design

--- frontend/javaScript/index.js
+++ frontend/javaScript/index.js
@@ -1,3 +1,5 @@
-document.getElementById('enter-button').addEventListener('click', function() {
-    location.href = '../frontend/html/home.html';
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('enter-button').addEventListener('click', () => {
+        location.href = '/frontend/html/home.html';
+    });
 });
